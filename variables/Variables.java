1a60ef1beeb07a9cd21508704b30f7899e898324
joshthecomposer <110321887+joshthecomposer@users.noreply.github.com>
2023-02-03 14:47:16 -0500

    init commit

---
+++ variables/Variables.java
@@ -0,0 +1,10 @@
+class Variables {
+    public static void main(String[] args) {
+        String firstName = "Joshua";
+        firstName = "Janiel";
+
+        double price1 = 3;
+        double price2 = price1;
+        double total = 0;
+    }
+}
