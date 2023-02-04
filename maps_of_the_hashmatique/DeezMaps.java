1769476d7bdde38b671ffc36b7a2c0e44c4f7ddc
Joshua Wise <joshuawise@joshuas-mbp.myfiosgateway.com>
2023-02-03 22:31:25 -0500

    hashmaps

---
+++ maps_of_the_hashmatique/DeezMaps.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Set;
+class DeezMaps {
+    public static void main(String[] asses) {
+        HashMap<String, String> trackList = new HashMap<String, String>();
+        trackList.put("Twinkle Twinkle", "LELELELE...");
+        trackList.put("ABCs", "A B C D E F G...");
+        trackList.put("BORN IN THE USA", "BOOOOOORN IN THE USAAAAAAAA.....");
+        trackList.put("Your own personal jesus", "DADADADADAD....");
+
+        String oneSong = trackList.get("ABCs");
+        System.out.println(oneSong);
+
+        Set<String> songs = trackList.keySet();
+        System.out.println(songs);
+        for(String key : songs) {
+            System.out.println(key + " : " + trackList.get(key));
+        }
+    }
+}
