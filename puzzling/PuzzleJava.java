3867f94b01b452a74dd24dcea9c12490f70dba0f
Joshua Wise <joshuawise@joshuas-mbp.myfiosgateway.com>
2023-02-03 22:03:34 -0500

    arraylisst stuff

--- puzzling/PuzzleJava.java
+++ puzzling/PuzzleJava.java
@@ -1,3 +1,51 @@
+import java.util.Random;
+import java.util.ArrayList;
+
 public class PuzzleJava {
-    
+    Random rd = new Random();
+
+    ArrayList<Integer> getTenRolls() {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+
+        for(int i = 0; i < 10; i++) {
+            int n = rd.nextInt(21 - 1) + 1;
+            result.add(n);
+        }
+        return result;
+    }
+
+    char getRandomLetter() {
+        char[] alphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        int n = rd.nextInt(alphabet.length);
+        return alphabet[n];
+    }
+    String generatePassword() {
+        String result = new String();
+        for(int i = 0; i < 8; i++) {
+            result += getRandomLetter();
+        }
+        return result;
+    }
+
+    String[] getNewPasswordSet(int input) {
+        String[] result = new String[input];
+
+        for (int i=0; i < result.length; i++) {
+            result[i] = generatePassword();
+        }
+        return result;
+    }
+
+    ArrayList<Integer> shuffleArray(ArrayList<Integer> input) {
+        for (int i = 0; i < input.size(); i++) {
+            int n = rd.nextInt(10-1) + 1;
+
+            Integer temp = input.get(i);
+
+            input.set(i, input.get(n));
+            input.set(n, temp);
+        }
+        return input;
+    }
+
 }
--- puzzling/testPuzzle.java
+++ puzzling/testPuzzle.java
@@ -1,6 +1,30 @@
+import java.util.ArrayList;
 class TestPuzzle {
     public static void main(String[] args) {
+
         PuzzleJava pj = new PuzzleJava();
         
+        // int[] tenRolls = pj.getTenRolls(10);
+        // for (int i : tenRolls) {
+        //     System.out.println(i);
+        // }
+
+        // char randomLetter = pj.getRandomLetter();
+        // System.out.println(randomLetter);
+
+        // String password = pj.generatePassword();
+        // System.out.println(password);
+
+        // String[] set = pj.getNewPasswordSet(300);
+        // for (String i : set) {
+        //     System.out.println(i);
+        // }
+
+        ArrayList<Integer> input = pj.getTenRolls();
+        System.out.println(input);
+        for (int i = 0; i < 30; i++) {
+            System.out.println(pj.shuffleArray(input));
+        }
+
     }
 }
